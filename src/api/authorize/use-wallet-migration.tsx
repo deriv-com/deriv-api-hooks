68db81d48690146634cdf1c8fae14199a749187e
Thisyahlen <thisyahlen@regentmarkets.com>
2024-03-22 15:13:29 +0800

    fix: typecheck in useWalletMigration

--- src/api/authorize/use-wallet-migration.tsx
+++ src/api/authorize/use-wallet-migration.tsx
@@ -1,11 +1,6 @@
 import { useAuthorizeQuery } from '../../base';
-import { TSocketQueryResults } from '../../base/use-query';
-import { TSocketResponseData } from '../../types/api.types';
 
-export const useWalletMigration = (): { data: TSocketResponseData<'wallet_migration'>['wallet_migration'] } & Omit<
-    TSocketQueryResults<'wallet_migration'>,
-    'data'
-> => {
+export const useWalletMigration = () => {
     const { data, ...rest } = useAuthorizeQuery({ name: 'wallet_migration' });
 
     return {

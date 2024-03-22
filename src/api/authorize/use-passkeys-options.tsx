798f3830c4778a6eb5b68f52e7afa4411044cb00
Thisyahlen <thisyahlen@regentmarkets.com>
2024-03-22 11:09:39 +0800

    chore: remove omit type

--- src/api/authorize/use-passkeys-options.tsx
+++ src/api/authorize/use-passkeys-options.tsx
@@ -1,11 +1,6 @@
 import { useAuthorizeQuery } from '../../base';
-import { TSocketQueryResults } from '../../base/use-query';
-import { TSocketResponseData } from '../../types/api.types';
 
-export const usePasskeysOptions = (): { data: TSocketResponseData<'passkeys_options'>['passkeys_options'] } & Omit<
-    TSocketQueryResults<'passkeys_options'>,
-    'data'
-> => {
+export const usePasskeysOptions = () => {
     const { data, ...rest } = useAuthorizeQuery({ name: 'passkeys_options' });
 
     return {
--- src/api/authorize/use-passkeys-register_options.tsx
+++ src/api/authorize/use-passkeys-register_options.tsx
@@ -1,14 +1,10 @@
 import { useAuthorizeQuery } from '../../base';
-import { TSocketQueryResults } from '../../base/use-query';
-import { TSocketResponseData } from '../../types/api.types';
 
-export const usePasskeysRegisterOptions = (): {
-    data: TSocketResponseData<'passkeys_register_options'>['passkeys_register_options'];
-} & Omit<TSocketQueryResults<'passkeys_register_options'>, 'data'> => {
+export const usePasskeysRegisterOptions = () => {
     const { data, ...rest } = useAuthorizeQuery({ name: 'passkeys_register_options' });
 
     return {
-        data: data!.passkeys_register_options,
+        data: data?.passkeys_register_options,
         ...rest,
     };
 };

7f50206ee29acf0e96f133210c96e0a6cd9ef202
yashim-deriv <yashim@regentmarkets.com>
2024-03-26 11:29:24 +0800

    release: v0.03

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { AppDataProvider } from "../src/context";
-import { SandboxPage } from "./sandbox-page";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { AppDataProvider } from '../src/context';
+import { SandboxPage } from './sandbox-page';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <AppDataProvider>
             <SandboxPage />

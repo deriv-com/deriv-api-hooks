33199643230bed62e0f1f8c9aba0b23a7b6db0b7
Thisyahlen <thisyahlen@regentmarkets.com>
2024-04-01 15:44:14 +0800

    chore: remove ...props

--- src/api/mutation/use-mt5-new-account.tsx
+++ src/api/mutation/use-mt5-new-account.tsx
@@ -1,7 +1,7 @@
 import { useMutation } from '../../base';
 
-export const useMT5NewAccount = ({ ...props }) => {
-    const { data, ...rest } = useMutation({ name: 'mt5_new_account', ...props });
+export const useMT5NewAccount = () => {
+    const { data, ...rest } = useMutation({ name: 'mt5_new_account' });
 
     return {
         data: data?.mt5_new_account,

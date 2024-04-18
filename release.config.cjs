dcd0d64861b2366e66df0a5e67038b23bb403088
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-04-18 11:43:42 +0800

    ci: add semantic release

---
+++ release.config.cjs
@@ -0,0 +1,55 @@
+const transform = require("./release.utils.cjs");
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+	branches: ["master"],
+	plugins: [
+		[
+			"@semantic-release/commit-analyzer",
+			{
+				releaseRules: [
+					{ type: "feat", release: "minor" },
+					{ type: "fix", release: "patch" },
+					{ type: "chore", release: "patch" },
+					{ type: "build", release: "patch" },
+					{ type: "ci", release: "patch" },
+					{ type: "docs", release: "patch" },
+					{ type: "style", release: "patch" },
+					{ type: "perf", release: "patch" },
+					{ type: "test", release: "patch" },
+					{ type: "refactor", release: "patch" },
+				],
+				parserOpts: {
+					noteKeywords: ["BREAKING", "BREAKING CHANGE", "BREAKING CHANGES"],
+				},
+			},
+		],
+		[
+			"@semantic-release/release-notes-generator",
+			{
+				parserOpts: {
+					mergePattern: /^Merge pull request #(\d+) from (.*)$/,
+					mergeCorrespondence: ["id", "source"],
+				},
+				writerOpts: {
+					transform: transform,
+				},
+			},
+		],
+		[
+			"@semantic-release/changelog",
+			{
+				changelogFile: "docs/CHANGELOG.md",
+			},
+		],
+		"@semantic-release/npm",
+		[
+			"@semantic-release/git",
+			{
+				assets: ["docs/CHANGELOG.md", "package.json"],
+			},
+		],
+		"@semantic-release/github",
+	],
+};

4d7eb615e96755c75c92ac5d78fb4ca3ab56c334
Thisyahlen <thisyahlen@regentmarkets.com>
2024-04-23 17:09:11 +0800

    chore: add useExchangeRates hook

--- src/api/subscription/index.ts
+++ src/api/subscription/index.ts
@@ -2,5 +2,6 @@ import { useP2PAdvertiserInfo } from './use-p2p-advertiser-info';
 import { useP2PSettings } from './use-p2p-settings';
 import { useP2POrderList } from './use-p2p-order-list';
 import { useP2POrderInfo } from './use-p2p-order-info';
+import { useExchangeRates } from './use-exchange-rates';
 
-export { useP2PAdvertiserInfo, useP2PSettings, useP2POrderList, useP2POrderInfo };
+export { useExchangeRates, useP2PAdvertiserInfo, useP2PSettings, useP2POrderList, useP2POrderInfo };

f87d9671f857da28dcc984fbe3fcea800f4f89de
Thisyahlen <thisyahlen@regentmarkets.com>
2024-04-24 09:42:23 +0800

    chore: Add default value to enabled flag because its undefined leading to api calls being made before authorize

--- src/base/use-authorize-query.tsx
+++ src/base/use-authorize-query.tsx
@@ -4,10 +4,12 @@ import { TSocketQueryOptions, useQuery } from './use-query';
 
 export const useAuthorizeQuery = <T extends TSocketEndpointNames>({
     name,
-    enabled,
+    enabled = true,
     ...rest
 }: TSocketQueryOptions<T>) => {
     const { isAuthorized } = useAuthData();
+
+    console.log(enabled, isAuthorized);
 
     return useQuery<T>({
         name,

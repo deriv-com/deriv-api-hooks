d1c7cdf4d0c6f4645d6b6aae75684f86c924c632
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-04-29 16:08:50 +0800

    chore: add enabled flag

--- src/api/mutation/use-p2p-advertiser-payment-methods.tsx
+++ src/api/mutation/use-p2p-advertiser-payment-methods.tsx
@@ -1,13 +1,37 @@
+import { useCallback, useEffect } from 'react';
 import { useMutation } from '../../base';
 import { AugmentedMutationOptions } from '../../base/use-mutation';
 
+type TMutateParams = Parameters<ReturnType<typeof useMutation<"p2p_advertiser_payment_methods">>["mutate"]>[0];
+
 export const useP2PAdvertiserPaymentMethods = ({
     ...props
-}: Omit<AugmentedMutationOptions<'p2p_advertiser_payment_methods'>, 'name'> = {}) => {
-    const { data, ...rest } = useMutation({ name: 'p2p_advertiser_payment_methods', ...props });
+}: Omit<AugmentedMutationOptions<'p2p_advertiser_payment_methods'>, 'name'> & { enabled?: boolean } = {}) => {
+    const { data, mutate, ...rest } = useMutation({ name: 'p2p_advertiser_payment_methods', ...props });
+
+    useEffect(() => {
+        if (props.enabled) {
+            mutate({})
+        }
+    }, []);
+
+    const create = useCallback((payload: NonNullable<TMutateParams["create"]>, loginid?: TMutateParams["loginid"]) => {
+        mutate({ create: payload, loginid });
+    }, [mutate])
+
+    const update = useCallback((payload: NonNullable<TMutateParams["update"]>, loginid?: TMutateParams["loginid"]) => {
+        mutate({ update: payload, loginid });
+    }, [mutate])
+
+    const deletePaymentMethod = useCallback((payload: NonNullable<TMutateParams["delete"]>, loginid?: TMutateParams["loginid"]) => {
+        mutate({ delete: payload, loginid });
+    }, [mutate])
 
     return {
         data: data?.p2p_advertiser_payment_methods,
+        create,
+        update,
+        deletePaymentMethod,
         ...rest,
     };
 };

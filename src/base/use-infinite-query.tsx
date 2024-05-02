c94c6ccd33d4ab78a11a6e411fd0d310c3c6e567
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-05-02 13:59:56 +0800

    chore: undo type change

--- src/base/use-infinite-query.tsx
+++ src/base/use-infinite-query.tsx
@@ -10,7 +10,7 @@ import { useAPI, useAuthData } from './use-context-hooks';
 export type TPaginatedQueryOptions<T extends TSocketPaginateableEndpointNames> = {
     name: T;
     payload?: TSocketRequestPayload<T>;
-    queryKey?: unknown[];
+    queryKey?: string[];
 } & Partial<Omit<UseInfiniteQueryOptions<TSocketResponseData<T>, TSocketError<T>>, 'queryKey' | 'queryFn' | 'select'>>;
 
 export const useInfiniteQuery = <T extends TSocketPaginateableEndpointNames>({

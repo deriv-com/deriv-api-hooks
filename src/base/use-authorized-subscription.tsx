3f5bc7926685a4db5b44321c7fe4dee7dfa83e2a
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-05-02 18:00:04 +0800

    chore: add isauthorizing check

--- src/base/use-authorized-subscription.tsx
+++ src/base/use-authorized-subscription.tsx
@@ -4,17 +4,17 @@ import { useAuthData } from "./use-context-hooks";
 import { useSubscribe } from "./use-subscribe";
 
 export const useAuthorizedSubscription = <T extends TSocketSubscribableEndpointNames>(name: T) => {
-    const { isAuthorized, activeLoginid } = useAuthData();
+    const { isAuthorized, activeLoginid, isAuthorizing } = useAuthData();
     const authTimeoutId = useRef<ReturnType<typeof setTimeout>>();
     const { subscribe, ...rest } = useSubscribe(name);
     const AUTH_TIMEOUT_DURATION = 400;
     const authorizedSubscribe = async (payload: Parameters<typeof subscribe>[0]) => {
-        if (isAuthorized && activeLoginid) {
+        if (isAuthorized && activeLoginid && !isAuthorizing) {
             subscribe(payload);
         } else {
             await new Promise((resolve, reject) => {
                 authTimeoutId.current = setTimeout(() => {
-                    if (isAuthorized && activeLoginid) {
+                    if (isAuthorized && activeLoginid && !isAuthorizing) {
                         clearTimeout(authTimeoutId.current);
                         subscribe(payload).then(resolve).catch(reject);
                     }

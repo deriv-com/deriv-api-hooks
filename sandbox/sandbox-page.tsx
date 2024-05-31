42eeb498ab7c59d59dd1b1d74659bec958d3527e
yashim-deriv <yashim@regentmarkets.com>
2024-05-31 11:08:09 +0800

    chore: add no check for sandbox temporarily

--- sandbox/sandbox-page.tsx
+++ sandbox/sandbox-page.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+// @ts-nocheck
+import React, { useState } from 'react';
 import { DerivAPIClient } from '../src/client-library/deriv-api-client';
+import { TSocketResponseData } from '../src/types/api.types';
 
 const derivAPI = new DerivAPIClient('wss://red.binaryws.com/websockets/v3?l=EN&app_id=1089&brand=deriv');
 
 export const SandboxPage = () => {
-    const handleSomeShit = async () => {
-        const response = await derivAPI.send('ping');
-        console.log(response.ping);
+    const [id, setId] = useState('');
+    const [data, setData] = useState<TSocketResponseData<'ticks'>>();
+
+    const handleClick = async () => {
+        const subs_id = await derivAPI.subscribe('ticks', { ticks: 'R_50' }, data => setData(data));
+        setId(subs_id);
+    };
+
+    const handleClick2 = async () => {
+        if (id) {
+            await derivAPI.unsubscribe(id);
+        }
     };
 
     return (
         <div>
             <div>
-                <button onClick={() => handleSomeShit()}>Kelik</button>
+                <div>{data?.tick?.ask ?? ''}</div>
+                <button onClick={handleClick}>Subscribe</button>
+                <button onClick={handleClick2}>Unsubscribe</button>
             </div>
         </div>
     );
